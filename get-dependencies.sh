da71205269aaec02c3c14f7d2012ed8e2113b98d
Link4Electronics <link4electronics@gmail.com>
2026-04-03 06:17:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,37 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	cmake		    \
+	kvantum 	    \
+	lxqt-qtplugin   \
+    pipewire-audio  \
+    pipewire-jack   \
+	qt6-declarative \
+	qt6-location    \
+	qt6-webchannel  \
+	qt6-webengine   \
+	qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building WhatSie..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/keshavbhatt/whatsie"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of WhatSie..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./whatsie
+else
+	echo "Making stable build of WhatSie..."
+	VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+	git clone --branch v"$VERSION" --single-branch --depth 1 "$REPO" ./whatsie
+fi
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cmake -S ./whatsie -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/usr" -B build
+cmake --build build -j$(nproc)
+cmake --install build
